5b263afe34c87f4b4f39fc5c39ed2d77c3c546e9
ariellezcano <ariellezcano1995@gmail.com>
2023-12-26 12:45:55 -0300

    ...

--- src/pages/compartido/sidebar/sidebar.tsx
+++ src/pages/compartido/sidebar/sidebar.tsx
@@ -10,20 +10,11 @@ function Sidebars() {
         <h3><b><em><u>Menú</u></em></b></h3>
         </div>
         
-        {/* <SubMenu label="MENÚ">
-        <MenuItem> Usuarios </MenuItem>
-          <MenuItem> Categorias </MenuItem>
-          <MenuItem> Productos </MenuItem>
-        </SubMenu> */}
+       
         <MenuItem component={<Link to="/usuarios" />}><i className="fa-solid  fa-users"></i>&nbsp; <h6>Usuarios</h6> </MenuItem>
         <MenuItem component={<Link to="/categorias" />}><i className="fa-solid fa-list"></i>&nbsp; <h6>Categorías</h6> </MenuItem>
         <MenuItem component={<Link to="/productos" />}><i className="fa-brands fa-product-hunt"></i>&nbsp; <h6>Productos</h6> </MenuItem>
-        {/* <MenuItem component={<Link to="/documentation" />}>
-          {" "}
-          Documentation
-        </MenuItem>
-        <MenuItem component={<Link to="/calendar" />}> Calendar</MenuItem>
-        <MenuItem component={<Link to="/e-commerce" />}> E-commerce</MenuItem> */}
+        
       </Menu>
     </Sidebar>
   );
